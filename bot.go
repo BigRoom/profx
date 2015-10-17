86ffaba339c991594519224ded00aace60784d7c
Harrison <harrison@theshoebridges.com>
2015-10-17 19:22:02 +1100

    Add RPC integration into vision

--- bot.go
+++ bot.go
@@ -3,13 +3,17 @@ package main
 import (
 	"log"
 	"net/rpc"
+	"time"
 
+	"github.com/bigrooms/vision/tunnel"
 	"github.com/nickvanw/ircx"
 	"github.com/paked/configure"
 	"github.com/sorcix/irc"
 )
 
 var (
+	client *rpc.Client
+
 	conf = configure.New()
 
 	name     = conf.String("name", "roomer", "The nick of your bot")
@@ -18,13 +22,14 @@ var (
 )
 
 func main() {
+	var err error
 	conf.Use(configure.NewFlag())
 	conf.Use(configure.NewEnvironment())
 
 	conf.Parse()
 
 	bot := ircx.Classic(*server, *name)
-	if err := bot.Connect(); err != nil {
+	if err = bot.Connect(); err != nil {
 		log.Panicln("Unable to connect to that IRC server", err)
 	}
 
@@ -32,11 +37,32 @@ func main() {
 	bot.HandleFunc(irc.PING, pingHandler)
 	bot.HandleFunc(irc.PRIVMSG, msgHandler)
 
+	client, err = rpc.DialHTTP("tcp", "localhost:6060")
+	if err != nil {
+		log.Panicln("Unable to connect: ", err)
+	}
+
 	bot.HandleLoop()
 }
 
 func msgHandler(s ircx.Sender, m *irc.Message) {
 	log.Println(m.Params, m.Trailing)
+
+	var reply tunnel.MessageReply
+	args := tunnel.MessageArgs{
+		From:    m.Name,
+		Content: m.Trailing,
+		Time:    time.Now(),
+	}
+
+	err := client.Call("Message.Dispatch", &args, &reply)
+	if err != nil {
+		log.Panicln("Unable to send: ", err)
+	}
+
+	if !reply.OK {
+		log.Println("Was not given the OK")
+	}
 }
 
 func registerHandler(s ircx.Sender, m *irc.Message) {
